f099bce063ad365dfb0e53e96a34c10e0901593d
CBHwang010 <Ianhuangcb@gmail.com>
2024-05-02 22:11:52 -0400

    Map

--- app/src/main/java/com/example/testing/MainActivity.java
+++ app/src/main/java/com/example/testing/MainActivity.java
@@ -3,19 +3,28 @@ package com.example.testing;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
+
+    private GoogleMap gMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        mapFragment.getMapAsync(this);
 
         Button login = (Button) findViewById(R.id.loginButton);
 
@@ -36,5 +45,10 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+
     }
 }
